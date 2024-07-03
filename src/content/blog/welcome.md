cc56147dd85da8c4d47ed5b103b00ddaa90612ee
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-03 19:51:29 +1000

    Update env, index page, and posts page with new features.

---
+++ src/content/blog/welcome.md
@@ -0,0 +1,8 @@
+---
+title: "Welcome, nothing to see here!"
+pubDate: 2024-07-03
+---
+
+Welcome, nothing to see here, _yet_!
+
+Hi, I'm aixoio! I love to program and I wanted a place to publish my work so I made this blog.
---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+    }),
+});
+
+export const collections = { blog };
